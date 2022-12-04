6b2375b85d214719de96d546e80719ddc456c630
haruki yamaguchi <hrk1106hasu@gmail.com>
2022-12-04 19:01:48 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,8 @@ import './App.css';
 import { InputTask } from './components/inputTask';
 import { ChangePage } from './components/changePage';
 import theme from './theme/theme';
-// import { auth, provider } from "./firebase";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { signInWithPopup, signOut } from 'firebase/auth';
-
 
 function App() {
-
-  // const [user]=useAuthState(auth);
-
-  // const signIn=()=>{
-  //   signInWithPopup(auth,provider)
-  //   .catch((err)=>{
-  //     alert(err.message);
-  //   })
-  // }
 
   return (
     <RecoilRoot>
